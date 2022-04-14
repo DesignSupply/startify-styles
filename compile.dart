8a0c6149efb30e373285bc93a7e6e473b16d3aab
DesignSupply <ogawa@designsupply-web.com>
2022-04-14 15:29:17 +0900

    [fix] コンパイル処理の修正

--- compile.dart
+++ compile.dart
@@ -3,11 +3,11 @@ import 'dart:convert';
 import 'package:sass/sass.dart' as sass;
 import 'package:source_maps/source_maps.dart';
 
-void main(List<String> arguments) {
+void main(List<String> source) {
   var outputStyle = sass.OutputStyle.compressed;
-  var result = sass.compileToResult(arguments[0], style: outputStyle, sourceMap: true, charset: false);
-  result.sourceMap.targetUrl = arguments[1];
-  result.sourceMap.sourceRoot = arguments[0];
-  new File(arguments[1]).writeAsStringSync(result.css + '/*# sourceMappingURL=main.min.css.map */');
-  new File(arguments[1] + '.map').writeAsStringSync(json.encode(result.sourceMap));
+  var result = sass.compileToResult(source[0], style: outputStyle, sourceMap: true, charset: false);
+  result.sourceMap.targetUrl = 'css/main.min.css';
+  result.sourceMap.sourceRoot = source[0];
+  new File('css/main.min.css').writeAsStringSync(result.css + '/*# sourceMappingURL=main.min.css.map */');
+  new File('css/main.min.css.map').writeAsStringSync(json.encode(result.sourceMap));
 }
