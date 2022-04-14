d891fa1cf450e340d61ece3a8cc79fb99405f978
DesignSupply <ogawa@designsupply-web.com>
2022-04-14 15:57:20 +0900

    [fix] 各種設定ファイルの修正

--- compile.dart
+++ compile.dart
@@ -6,8 +6,6 @@ import 'package:source_maps/source_maps.dart';
 void main(List<String> source) {
   var outputStyle = sass.OutputStyle.compressed;
   var result = sass.compileToResult(source[0], style: outputStyle, sourceMap: true, charset: false);
-  result.sourceMap.targetUrl = 'css/main.min.css';
-  result.sourceMap.sourceRoot = source[0];
   new File('css/main.min.css').writeAsStringSync(result.css + '/*# sourceMappingURL=main.min.css.map */');
   new File('css/main.min.css.map').writeAsStringSync(json.encode(result.sourceMap));
 }
